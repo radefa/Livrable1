d6d06283ec0b25e85f01930cc587f52c109d8c6e
Pythagore Deffo <deffopa8@yaoo.fr>
2016-11-01 10:53:30 -0400

    Grille fonctionnel

--- Livrable1_test/app/src/main/java/com/example/dpyth/livrable1_test/MainActivity.java
+++ Livrable1_test/app/src/main/java/com/example/dpyth/livrable1_test/MainActivity.java
@@ -3,17 +3,27 @@ package com.example.dpyth.livrable1_test;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.GridLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView img=(ImageView)findViewById(R.id.black_king_st);
-        Drawable myDrawable = getResources().getDrawable(R.drawable.black_king_st);
-        img.setImageDrawable(myDrawable);
+
+
+       /* GridLayout grd = (GridLayout)findViewById(R.id.GridLayout1);
+        grd.setPadding(0, 0, 0, 0);
+        grd.setUseDefaultMargins(false);
+        grd.setAlignmentMode(GridLayout.ALIGN_MARGINS);*/
+    }
+    public void fonctionCouleur ()
+    {
+        GridLayout grd = (GridLayout)findViewById(R.id.GridLayout1);
+
+
 
     }
 }
